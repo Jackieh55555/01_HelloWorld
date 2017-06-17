cf988040c630ed8a1279fec66e126b601efb44f4
Jackieh55555 <jackie.huang.ca@gmail.com>
2017-06-16 22:40:08 -0400

    BIT2400

---
+++ 01_HelloWorld.cpp
@@ -0,0 +1,21 @@
+#include "stdio.h"
+#include <iostream>
+
+using namespace std;
+
+// define constants (ie. label)
+#define LENGTH_USERNAME  40  // reserve 40 characters to store the user's name
+
+
+//===== Main Program =====
+void main()
+{
+	char userName[40];
+
+	// Prompt for user's name
+	cout << "What is your name? ";
+	cin >> userName;
+
+	// print out hello message
+	cout << "Hello, " << userName << endl;
+}
